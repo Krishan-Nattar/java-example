de06550d9e32543799e188b1c58415aae33e62b1
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-19 22:49:30 -0700

    updated version

---
+++ example/src/com/lambda/Greeter.java
@@ -0,0 +1,17 @@
+package com.lambda;
+
+public class Greeter
+{
+    public String sayHello()
+    {
+        return "Hellow World";
+    }
+
+
+    public String sayHello(String name)
+    {
+        return "Hello, " + name;
+    }
+
+
+}
--- example/src/com/lambda/Main.java
+++ example/src/com/lambda/Main.java
@@ -2,9 +2,13 @@ package com.lambda;
 
 public class Main
 {
-
+    //sdk = software development kit
+    //jdk is an sdk, for java
     public static void main(String[] args)
     {
-        System.out.println("Running...");
+//        System.out.println("Running...");
+        Greeter hello = new Greeter();
+        System.out.println(hello.sayHello("Krishan"));
+
     }
 }
